53fb4ecfeb8406c5a5102997abedf256acd29458
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-17 04:34:52 -0500

    Create 168-ExcelSheetColumnTitle

---
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.py
@@ -0,0 +1,9 @@
+# Last updated: 7/17/2026, 3:04:51 PM
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        result = []
+        while columnNumber > 0:
+            columnNumber -= 1
+            result.append(chr(columnNumber % 26 + ord('A')))
+            columnNumber //= 26
+        return ''.join(result[::-1])
