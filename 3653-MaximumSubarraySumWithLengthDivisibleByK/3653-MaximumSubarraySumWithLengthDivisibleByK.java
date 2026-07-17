2a0279c114dd8972b1cc70af805adb28a71b9f3d
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-17 04:31:59 -0500

    Create 3653-MaximumSubarraySumWithLengthDivisibleByK

---
+++ 3653-MaximumSubarraySumWithLengthDivisibleByK/3653-MaximumSubarraySumWithLengthDivisibleByK.java
@@ -0,0 +1,19 @@
+// Last updated: 7/17/2026, 3:01:58 PM
+class Solution {
+    public long maxSubarraySum(int[] nums, int k) {
+        long[] minPref = new long[k];
+        Arrays.fill(minPref, Long.MAX_VALUE / 2);
+        minPref[k - 1] = 0;
+        
+        long sum = 0, res = Long.MIN_VALUE;
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int r = i % k;
+            res = Math.max(res, sum - minPref[r]);
+            minPref[r] = Math.min(minPref[r], sum);
+        }
+        
+        return res;
+    }
+}
