e2a4e7ec74d251ed67a28d0297325a3210b47742
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-17 09:10:58 -0500

    File updated at 7/17/2026, 9:10:58 PM

--- 6-ZigzagConversion/6-ZigzagConversion.java
+++ 6-ZigzagConversion/6-ZigzagConversion.java
@@ -1,25 +1,52 @@
-// Last updated: 7/17/2026, 9:10:03 PM
+// Last updated: 7/17/2026, 9:10:58 PM
 1class Solution {
-2    public String convert(String s, int numRows) {
-3        if(numRows== 1 || numRows >= s.length()) {
-4            return s;
+2    public int myAtoi(String s) {
+3        if (s == null || s.length() == 0) {
+4            return 0;
 5        }
-6        StringBuilder sb = new StringBuilder();
-7        int n = s.length();
-8        int cycle = 2 * numRows - 2;
-9        for (int i = 0; i < numRows; i++) {
-10            int j = i;
-11            while (j < n) {
-12                sb.append(s.charAt(j));
-13                if (i != 0 && i != numRows - 1) {
-14                    int midIndex = j + cycle - 2 * i;
-15                    if (midIndex < n) {
-16                        sb.append(s.charAt(midIndex));
-17                    }
-18                }
-19                j += cycle;
-20            }
-21        }
-22        return sb.toString();
-23    }
-24}
+6
+7        int index = 0;
+8        int n = s.length();
+9        int sign = 1;
+10        int result = 0;
+11
+12        // 1. Discard leading whitespaces
+13        while (index < n && s.charAt(index) == ' ') {
+14            index++;
+15        }
+16
+17        // If the string is empty or only had spaces
+18        if (index == n) {
+19            return 0;
+20        }
+21
+22        // 2. Check for sign
+23        if (s.charAt(index) == '+' || s.charAt(index) == '-') {
+24            sign = (s.charAt(index) == '-') ? -1 : 1;
+25            index++;
+26        }
+27
+28        // 3. Convert digits and handle overflow
+29        while (index < n) {
+30            char currChar = s.charAt(index);
+31            
+32            // Stop if it's a non-digit character
+33            if (currChar < '0' || currChar > '9') {
+34                break;
+35            }
+36
+37            int digit = currChar - '0';
+38
+39            // 4. Check overflow/underflow bounds before updating result
+40            if (result > Integer.MAX_VALUE / 10 || 
+41               (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+42                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+43            }
+44
+45            result = result * 10 + digit;
+46            index++;
+47        }
+48
+49        return result * sign;
+50    }
+51}
