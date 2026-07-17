52f9391ae1210a75b3565d48a79cb8277455439c
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-17 09:53:12 -0500

    File updated at 7/17/2026, 9:53:12 PM

--- 18-4Sum/18-4Sum.java
+++ 18-4Sum/18-4Sum.java
@@ -1,50 +1,37 @@
-// Last updated: 7/17/2026, 9:50:28 PM
-import java.util.*;
-
-class Solution {
-    private List<List<Integer>> finalResult;
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        return new AbstractList<List<Integer>>() {
-            public List<Integer> get(int index) {
-                f();
-                return finalResult.get(index);
-            }
-
-            public int size() {
-                f();
-                return finalResult.size();
-            }
-
-            private void f() { 
-                List<List<Integer>> resultList = new ArrayList<>();
-                Set<List<Integer>> uniqueSet = new HashSet<>();
-                int n = nums.length;
-                Arrays.sort(nums);
-                for(int first = 0; first < n - 3; first++) {
-                    for(int second = first + 1; second < n - 2; second++) {
-                        long remainingTarget = (long) target - (long) nums[first] - (long) nums[second];
-                        int left = second + 1, right = n - 1;
-
-                        while(left < right) {
-                            if(nums[left] + nums[right] == remainingTarget) {
-                                uniqueSet.add(Arrays.asList(nums[left], nums[right], nums[first], nums[second]));
-                                while(left < right && nums[left] == nums[left + 1]) 
-                                    left++;
-                                while(left < right && nums[right] == nums[right - 1]) 
-                                    right--;
-                                left++;
-                                right--;
-                            } else if(nums[left] + nums[right] < remainingTarget) {
-                                left++;
-                            } else {
-                                right--;
-                            }
-                        }
-                    }
-                }
-                resultList.addAll(uniqueSet);
-                finalResult = resultList;
-            }
-        };
-    }
-}
+// Last updated: 7/17/2026, 9:53:12 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode removeNthFromEnd(ListNode head, int n) {
+13        // Create a dummy node to seamlessly handle edge cases (like removing the head node)
+14        ListNode dummy = new ListNode(0);
+15        dummy.next = head;
+16        
+17        ListNode fast = dummy;
+18        ListNode slow = dummy;
+19        
+20        // 1. Move the fast pointer so that there is a gap of n nodes between fast and slow
+21        for (int i = 0; i <= n; i++) {
+22            fast = fast.next;
+23        }
+24        
+25        // 2. Move fast to the end, maintaining the constant gap
+26        while (fast != null) {
+27            fast = fast.next;
+28            slow = slow.next;
+29        }
+30        
+31        // 3. slow is now right before the node to be deleted. Skip the target node.
+32        slow.next = slow.next.next;
+33        
+34        return dummy.next;
+35    }
+36}
