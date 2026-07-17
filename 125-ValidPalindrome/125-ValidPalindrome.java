ef3c161fe96e98a9d77470f8e09e00f08c85ca77
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-17 04:35:13 -0500

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,28 @@
+// Last updated: 7/17/2026, 3:05:12 PM
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
