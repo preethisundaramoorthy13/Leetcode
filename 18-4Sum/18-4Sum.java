61c9a19a4cd446577f72e842d2729711dfda1060
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-17 09:56:51 -0500

    File updated at 7/17/2026, 9:56:51 PM

--- 18-4Sum/18-4Sum.java
+++ 18-4Sum/18-4Sum.java
@@ -1,34 +1,49 @@
-// Last updated: 7/17/2026, 9:54:26 PM
-1import java.util.ArrayList;
-2import java.util.List;
-3
-4class Solution {
-5    public List<String> generateParenthesis(int n) {
-6        List<String> result = new ArrayList<>();
-7        // Start the recursive backtracking with 0 open and 0 close brackets
-8        backtrack(result, new StringBuilder(), 0, 0, n);
-9        return result;
-10    }
-11
-12    private void backtrack(List<String> result, StringBuilder current, int open, int close, int max) {
-13        // Base Case: If the string reaches the required length, add it to the list
-14        if (current.length() == max * 2) {
-15            result.add(current.toString());
-16            return;
-17        }
-18
-19        // Rule 1: We can add an opening parenthesis if we haven't reached the limit 'max'
-20        if (open < max) {
-21            current.append('(');
-22            backtrack(result, current, open + 1, close, max);
-23            current.deleteCharAt(current.length() - 1); // Backtrack
-24        }
-25
-26        // Rule 2: We can add a closing parenthesis if it doesn't exceed the number of open ones
-27        if (close < open) {
-28            current.append(')');
-29            backtrack(result, current, open, close + 1, max);
-30            current.deleteCharAt(current.length() - 1); // Backtrack
-31        }
-32    }
-33}
+// Last updated: 7/17/2026, 9:56:51 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11import java.util.PriorityQueue;
+12
+13class Solution {
+14    public ListNode mergeKLists(ListNode[] lists) {
+15        // Edge cases
+16        if (lists == null || lists.length == 0) {
+17            return null;
+18        }
+19        
+20        // Initialize Min-Heap based on node values
+21        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
+22        
+23        // Add the head node of each non-empty list to the heap
+24        for (ListNode head : lists) {
+25            if (head != null) {
+26                minHeap.add(head);
+27            }
+28        }
+29        
+30        // Dummy node to easily build the output list
+31        ListNode dummy = new ListNode(0);
+32        ListNode tail = dummy;
+33        
+34        // Process the heap until empty
+35        while (!minHeap.isEmpty()) {
+36            ListNode smallestNode = minHeap.poll();
+37            tail.next = smallestNode;
+38            tail = tail.next;
+39            
+40            // If there is a next node in that list, push it to the heap
+41            if (smallestNode.next != null) {
+42                minHeap.add(smallestNode.next);
+43            }
+44        }
+45        
+46        return dummy.next;
+47    }
+48}
