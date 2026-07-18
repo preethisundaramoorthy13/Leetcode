9ba5ae6d0be50972d7201ae30d83c02c262fb02b
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-18 09:01:06 -0500

    Create 44-WildcardMatching

---
+++ 44-WildcardMatching/44-WildcardMatching.java
@@ -0,0 +1,31 @@
+// Last updated: 7/18/2026, 9:01:06 PM
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+        int sIdx = 0, pIdx = 0;
+        int starIdx = -1, sTmpIdx = -1;
+
+        while (sIdx < sLen) {
+            if (pIdx < pLen && (p.charAt(pIdx) == '?' || p.charAt(pIdx) == s.charAt(sIdx))) {
+                sIdx++;
+                pIdx++;
+            } else if (pIdx < pLen && p.charAt(pIdx) == '*') {
+                starIdx = pIdx;
+                sTmpIdx = sIdx;
+                pIdx++;
+            } else if (starIdx != -1) {
+                pIdx = starIdx + 1;
+                sTmpIdx++;
+                sIdx = sTmpIdx;
+            } else {
+                return false;
+            }
+        }
+
+        while (pIdx < pLen && p.charAt(pIdx) == '*') {
+            pIdx++;
+        }
+
+        return pIdx == pLen;
+    }
+}
