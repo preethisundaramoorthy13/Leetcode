1557350154d7d6bd488ef0ba6841278c11b2f3c6
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-18 08:07:33 -0500

    File updated at 7/18/2026, 8:07:33 AM

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,31 +1,27 @@
-// Last updated: 7/18/2026, 8:05:35 AM
+// Last updated: 7/18/2026, 8:07:33 AM
 1class Solution {
-2    public boolean isMatch(String s, String p) {
-3        int sLen = s.length(), pLen = p.length();
-4        int sIdx = 0, pIdx = 0;
-5        int starIdx = -1, sTmpIdx = -1;
+2    public int jump(int[] nums) {
+3        if (nums.length <= 1) {
+4            return 0;
+5        }
 6
-7        while (sIdx < sLen) {
-8            if (pIdx < pLen && (p.charAt(pIdx) == '?' || p.charAt(pIdx) == s.charAt(sIdx))) {
-9                sIdx++;
-10                pIdx++;
-11            } else if (pIdx < pLen && p.charAt(pIdx) == '*') {
-12                starIdx = pIdx;
-13                sTmpIdx = sIdx;
-14                pIdx++;
-15            } else if (starIdx != -1) {
-16                pIdx = starIdx + 1;
-17                sTmpIdx++;
-18                sIdx = sTmpIdx;
-19            } else {
-20                return false;
+7        int jumps = 0;
+8        int currentEnd = 0;
+9        int farthest = 0;
+10
+11        for (int i = 0; i < nums.length - 1; i++) {
+12            farthest = Math.max(farthest, i + nums[i]);
+13            
+14            if (i == currentEnd) {
+15                jumps++;
+16                currentEnd = farthest;
+17                
+18                if (currentEnd >= nums.length - 1) {
+19                    break;
+20                }
 21            }
 22        }
 23
-24        while (pIdx < pLen && p.charAt(pIdx) == '*') {
-25            pIdx++;
-26        }
-27
-28        return pIdx == pLen;
-29    }
-30}
+24        return jumps;
+25    }
+26}
