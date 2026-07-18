174bb324c9631ccc8e9495bdf4664dd74863191f
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-18 09:16:00 -0500

    Create 51-NQueens

---
+++ 51-NQueens/51-NQueens.java
@@ -0,0 +1,55 @@
+// Last updated: 7/18/2026, 9:16:00 PM
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        boolean[] cols = new boolean[n];
+        boolean[] diag1 = new boolean[2 * n];
+        boolean[] diag2 = new boolean[2 * n];
+        backtrack(0, n, board, cols, diag1, diag2, result);
+        return result;
+    }
+
+    private void backtrack(int row, int n, char[][] board, boolean[] cols, boolean[] diag1, boolean[] diag2, List<List<String>> result) {
+        if (row == n) {
+            result.add(construct(board));
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            int d1 = row - col + n;
+            int d2 = row + col;
+            if (cols[col] || diag1[d1] || diag2[d2]) {
+                continue;
+            }
+
+            board[row][col] = 'Q';
+            cols[col] = true;
+            diag1[d1] = true;
+            diag2[d2] = true;
+
+            backtrack(row + 1, n, board, cols, diag1, diag2, result);
+
+            board[row][col] = '.';
+            cols[col] = false;
+            diag1[d1] = false;
+            diag2[d2] = false;
+        }
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> path = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            path.add(new String(board[i]));
+        }
+        return path;
+    }
+}
