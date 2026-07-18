79d7ddb7f176d4f056ce814bbe1b8c08b47aa7db
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-18 08:05:34 -0500

    File updated at 7/18/2026, 8:05:35 AM

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,37 +1,31 @@
-// Last updated: 7/18/2026, 8:02:53 AM
-class Solution {
-    public String multiply(String num1, String num2) {
-        if ("0".equals(num1) || "0".equals(num2))
-            return "0";
-        if ("1".equals(num1))
-            return num2;
-        if ("1".equals(num2))
-            return num1;
-
-        char[] c1 = num1.toCharArray();
-        char[] c2 = num2.toCharArray();
-        int m = c1.length, n = c2.length;
-
-        int[] vals = new int[m + n];
-
-        for (int i = m - 1; i >= 0; i--) {
-            int n1 = c1[i] - '0';
-            for (int j = n - 1; j >= 0; j--) {
-                int n2 = c2[j] - '0';
-                vals[i + j + 1] += n1 * n2;
-            }
-        }
-        int carry = 0;
-        for (int i = vals.length - 1; i >= 0; i--) {
-            int total = vals[i] + carry;
-            vals[i] = total % 10;
-            carry = total / 10;
-        }
-        StringBuilder sb = new StringBuilder(m + n);
-        int start = vals[0] == 0 ? 1 : 0;
-        for (int i = start; i < vals.length; i++) {
-            sb.append(vals[i]);
-        }
-        return sb.toString();
-    }
-}
+// Last updated: 7/18/2026, 8:05:35 AM
+1class Solution {
+2    public boolean isMatch(String s, String p) {
+3        int sLen = s.length(), pLen = p.length();
+4        int sIdx = 0, pIdx = 0;
+5        int starIdx = -1, sTmpIdx = -1;
+6
+7        while (sIdx < sLen) {
+8            if (pIdx < pLen && (p.charAt(pIdx) == '?' || p.charAt(pIdx) == s.charAt(sIdx))) {
+9                sIdx++;
+10                pIdx++;
+11            } else if (pIdx < pLen && p.charAt(pIdx) == '*') {
+12                starIdx = pIdx;
+13                sTmpIdx = sIdx;
+14                pIdx++;
+15            } else if (starIdx != -1) {
+16                pIdx = starIdx + 1;
+17                sTmpIdx++;
+18                sIdx = sTmpIdx;
+19            } else {
+20                return false;
+21            }
+22        }
+23
+24        while (pIdx < pLen && p.charAt(pIdx) == '*') {
+25            pIdx++;
+26        }
+27
+28        return pIdx == pLen;
+29    }
+30}
