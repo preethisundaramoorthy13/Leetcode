8a926858f5fe995541b13d7078a611340032f34b
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-18 05:07:52 -0500

    File updated at 7/18/2026, 5:07:52 PM

--- 36-ValidSudoku/36-ValidSudoku.java
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -1,28 +1,37 @@
-// Last updated: 7/18/2026, 5:06:43 PM
+// Last updated: 7/18/2026, 5:07:52 PM
 1class Solution {
-2    static {
-3        for (int index = 0; index < 100; index++)
-4            isValidSudoku(new char[9][9]);
-5    }
-6    public static boolean isValidSudoku(char[][] board) {
-7        int[] rowMask = new int[9];
-8        int[] columnMask = new int[9];
-9        int[] boxMask = new int[9];
-10
-11        for (int row = 0; row < 9; row++) {
-12            for (int column = 0; column < 9; column++) {
-13                if (board[row][column] == '.') continue;
-14                int bit = 1 << (board[row][column] - '1');
-15                int box = (row / 3) * 3 + column / 3;
-16
-17                if ((rowMask[row] & bit) != 0 || (columnMask[column] & bit) != 0 || (boxMask[box] & bit) != 0) return false;
-18                
-19                rowMask[row] |= bit;
-20                columnMask[column] |= bit;
-21                boxMask[box] |= bit;
-22            }
-23        }
-24
+2    public void solveSudoku(char[][] board) {
+3        solve(board);
+4    }
+5
+6    private boolean solve(char[][] board) {
+7        for (int i = 0; i < 9; i++) {
+8            for (int j = 0; j < 9; j++) {
+9                if (board[i][j] == '.') {
+10                    for (char c = '1'; c <= '9'; c++) {
+11                        if (isValid(board, i, j, c)) {
+12                            board[i][j] = c;
+13
+14                            if (solve(board)) {
+15                                return true;
+16                            } else {
+17                                board[i][j] = '.';
+18                            }
+19                        }
+20                    }
+21                    return false;
+22                }
+23            }
+24        }
 25        return true;
 26    }
-27}
+27
+28    private boolean isValid(char[][] board, int row, int col, char c) {
+29        for (int i = 0; i < 9; i++) {
+30            if (board[i][col] == c) return false;
+31            if (board[row][i] == c) return false;
+32            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+33        }
+34        return true;
+35    }
+36}
