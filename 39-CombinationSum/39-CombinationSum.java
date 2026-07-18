f8fe8b74831620ac7298c5e903d54cb6409ae31a
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-18 08:22:12 -0500

    File updated at 7/18/2026, 8:22:12 PM

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,38 +1,35 @@
-// Last updated: 7/18/2026, 8:08:42 AM
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
-        getPerm(nums,0,result);
-        return result;
-        
-    }
-    public /*List<List<Integer>>*/ void getPerm(int nums[],int index,/*int*/ List<List<Integer>> perm)//perm is a list not integer, so dont use int with list. also make the function void as not returning something
-    {
-        //the main idea is to swap each index with the each element in array, so we take index var for iterating over index and for element iteration we have variable i, now we will everytime iterate i from index to n, recall the state space tree, how we fill one position and swap other two, and move to next node and so on. and then we do backtrack, so we swap once again. the swap function will carry one array, and  two places with need to be swapped, index and i. so this is the code
-        int n=nums.length;
-        if(index==n)
-        {
-            List<Integer> current=new ArrayList<>();
-            for(int j=0;j<nums.length;j++)
-            {
-                current.add(nums[j]);//we are making here a list from array of num, cuz perm is list of list it will only take, list as input, so we firts ahve to convert nums to list.
-            }
-            perm.add(current);
-            return;
-        }
-        for(int i=index;i<n;i++)
-        {
-            swapNum(nums,index,i);
-            getPerm(nums,index+1,perm);
-            //backtrack swap
-            swapNum(nums,index,i);
-        }
-    }
-    public static void swapNum(int nums[],int index,int i)//here we are passing two integers, but we should pass one array and its two indices
-    {
-        
-        int temp=nums[index];
-        nums[index]=nums[i];
-        nums[i]=temp;
-    }
-}
+// Last updated: 7/18/2026, 8:22:12 PM
+1import java.util.ArrayList;
+2import java.util.Arrays;
+3import java.util.List;
+4
+5class Solution {
+6    public List<List<Integer>> permuteUnique(int[] nums) {
+7        List<List<Integer>> result = new ArrayList<>();
+8        Arrays.sort(nums);
+9        backtrack(nums, new boolean[nums.length], new ArrayList<>(), result);
+10        return result;
+11    }
+12
+13    private void backtrack(int[] nums, boolean[] used, List<Integer> current, List<List<Integer>> result) {
+14        if (current.size() == nums.length) {
+15            result.add(new ArrayList<>(current));
+16            return;
+17        }
+18
+19        for (int i = 0; i < nums.length; i++) {
+20            if (used[i]) {
+21                continue;
+22            }
+23            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+24                continue;
+25            }
+26
+27            used[i] = true;
+28            current.add(nums[i]);
+29            backtrack(nums, used, current, result);
+30            current.remove(current.size() - 1);
+31            used[i] = false;
+32        }
+33    }
+34}
