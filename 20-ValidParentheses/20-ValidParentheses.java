942b05617e30c893ad31a1351536668d14094135
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-18 09:01:39 -0500

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,18 @@
+// Last updated: 7/18/2026, 9:01:40 PM
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() % 2 == 1) return false;
+        return isValid(0, ' ', s.toCharArray()) > 0;
+    } 
+    private int isValid(int i, char last, char[] s) {
+        if(i == s.length) return i;
+        if(i > s.length) return - 1;
+        if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            int next = isValid(i + 1, s[i], s);
+            if(next < 0) return -1;
+            else return isValid(next + 1, last, s);
+        }
+        if(last == '(' && s[i] == ')' || last == '[' && s[i] == ']' || last == '{' && s[i] == '}') return i;
+        return -1;
+    }
+}
