3801218e53f6d575292a4728cf51ef3fc1bfd4f3
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-18 05:57:51 -0500

    File updated at 7/18/2026, 5:57:51 AM

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,27 +1,23 @@
-// Last updated: 7/18/2026, 5:57:20 AM
-1import java.util.ArrayList;
-2import java.util.List;
-3
-4class Solution {
-5    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-6        List<List<Integer>> result = new ArrayList<>();
-7        backtrack(candidates, target, 0, new ArrayList<>(), result);
-8        return result;
-9    }
-10
-11    private void backtrack(int[] candidates, int target, int index, List<Integer> current, List<List<Integer>> result) {
-12        if (target == 0) {
-13            result.add(new ArrayList<>(current));
-14            return;
-15        }
-16        if (target < 0 || index >= candidates.length) {
-17            return;
-18        }
-19        current.add(candidates[index]);
-20        backtrack(candidates, target - candidates[index], index, current, result);
-21       
-22        current.remove(current.size() - 1);
-23
-24        backtrack(candidates, target, index + 1, current, result);
-25    }
-26}
+// Last updated: 7/18/2026, 5:57:51 AM
+class Solution {
+    void fc(int[] can,int target,List<Integer>ans,List<List<Integer>>res,int sum,int index){
+        if(index==can.length){
+            if(sum==target){
+                res.add(new ArrayList(ans));
+            }
+            return;
+        }
+        if(sum+can[index]<=target){
+            ans.add(can[index]);
+            fc(can,target,ans,res,sum+can[index],index);
+            ans.remove(ans.size()-1);
+        }
+        fc(can,target,ans,res,sum,index+1);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer>ans=new ArrayList<>();
+        List<List<Integer>>res=new ArrayList<>();
+        fc(candidates,target,ans,res,0,0);
+        return res;
+    }
+}
