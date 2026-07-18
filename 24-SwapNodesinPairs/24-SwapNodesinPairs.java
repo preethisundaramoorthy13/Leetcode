5375a5107a272cce4759b6d904d9afbd79248bfe
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-18 02:04:39 -0500

    File updated at 7/18/2026, 2:04:40 AM

--- 24-SwapNodesinPairs/24-SwapNodesinPairs.java
+++ 24-SwapNodesinPairs/24-SwapNodesinPairs.java
@@ -1,4 +1,4 @@
-// Last updated: 7/18/2026, 2:02:26 AM
+// Last updated: 7/18/2026, 2:04:40 AM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,28 +10,46 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode swapPairs(ListNode head) {
-13        // Create a dummy node to point to the head of the list
-14        ListNode dummy = new ListNode(0);
-15        dummy.next = head;
-16        
-17        // This pointer tracks the node right before the pair we are swapping
-18        ListNode prev = dummy;
-19        
-20        // Make sure there are at least two nodes left to swap
-21        while (prev.next != null && prev.next.next != null) {
-22            ListNode first = prev.next;
-23            ListNode second = prev.next.next;
-24            
-25            // Adjusting pointers to swap the pair
-26            first.next = second.next;
-27            second.next = first;
-28            prev.next = second;
-29            
-30            // Move the prev pointer two steps ahead for the next pair
-31            prev = first;
-32        }
-33        
-34        return dummy.next;
-35    }
-36}
+12    public ListNode reverseKGroup(ListNode head, int k) {
+13        if (head == null || k == 1) return head;
+14        
+15        ListNode dummy = new ListNode(0);
+16        dummy.next = head;
+17        
+18        ListNode prevGroupEnd = dummy;
+19        ListNode curr = head;
+20        
+21        while (curr != null) {
+22            // Check if there are at least k nodes left to reverse
+23            ListNode groupEnd = prevGroupEnd;
+24            for (int i = 0; i < k; i++) {
+25                groupEnd = groupEnd.next;
+26                if (groupEnd == null) {
+27                    // Less than k nodes left, keep them as-is and finish
+28                    return dummy.next;
+29                }
+30            }
+31            
+32            // These will track our boundary positions
+33            ListNode nextGroupStart = groupEnd.next;
+34            ListNode groupStart = prevGroupEnd.next;
+35            
+36            // Reverse the current group of k nodes
+37            ListNode prev = nextGroupStart;
+38            ListNode p = groupStart;
+39            while (p != nextGroupStart) {
+40                ListNode nextNode = p.next;
+41                p.next = prev;
+42                prev = p;
+43                p = nextNode;
+44            }
+45            
+46            // Connect the reversed group back to the main list
+47            prevGroupEnd.next = groupEnd;
+48            prevGroupEnd = groupStart;
+49            curr = nextGroupStart;
+50        }
+51        
+52        return dummy.next;
+53    }
+54}
