1763e3bfd0976f745ed930fdef64de53b03755d8
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-18 08:01:05 -0500

    File updated at 7/18/2026, 8:01:06 AM

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,34 +1,54 @@
-// Last updated: 7/18/2026, 7:59:16 AM
-1import java.util.ArrayList;
-2import java.util.Arrays;
+// Last updated: 7/18/2026, 8:01:06 AM
+1import java.util.AbstractList;
+2import java.util.ArrayList;
 3import java.util.List;
 4
 5class Solution {
-6    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-7        List<List<Integer>> result = new ArrayList<>();
-8        Arrays.sort(candidates);
-9        backtrack(candidates, target, 0, new ArrayList<>(), result);
-10        return result;
-11    }
-12
-13    private void backtrack(int[] candidates, int target, int index, List<Integer> current, List<List<Integer>> result) {
-14        if (target == 0) {
-15            result.add(new ArrayList<>(current));
-16            return;
-17        }
-18
-19        for (int i = index; i < candidates.length; i++) {
-20            if (candidates[i] > target) {
-21                break;
-22            }
-23
-24            if (i > index && candidates[i] == candidates[i - 1]) {
-25                continue;
-26            }
-27
-28            current.add(candidates[i]);
-29            backtrack(candidates, target - candidates[i], i + 1, current, result);
-30            current.remove(current.size() - 1);
-31        }
-32    }
-33}
+6private void combinations2(int index, int[] arr, int target, List<Integer> subset, List<List<Integer>> res){
+7        if(target == 0){
+8            res.add(List.copyOf(subset));
+9            return;
+10        }
+11
+12        for(int i = index; i < arr.length; i++){
+13            if(i > index && arr[i] == arr[i-1]){
+14                continue;
+15            }
+16            if (arr[i] > target) {
+17                break;
+18            }
+19            subset.add(arr[i]);
+20            combinations2(i + 1, arr, target - arr[i], subset, res);
+21            subset.removeLast();
+22        }
+23    }
+24
+25    private void getResults(int[] arr, int sum, List<List<Integer>> result) {
+26        java.util.Arrays.sort(arr);
+27        combinations2(0, arr, sum, new ArrayList<>(), result);
+28    }
+29
+30    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+31        return new AbstractList<List<Integer>>() {
+32            List<List<Integer>> res;
+33
+34            private void init(){
+35                res = new java.util.ArrayList<>();
+36                getResults(candidates, target,res);
+37            }
+38
+39            @Override
+40            public List<Integer> get(int index) {
+41                return res.get(index);
+42            }
+43
+44            @Override
+45            public int size() {
+46                if(res == null){
+47                    init();
+48                }
+49                return res.size();
+50            }
+51        };
+52    }
+53}
