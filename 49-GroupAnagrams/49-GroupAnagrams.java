48a0ae01d5607499bf53f519e5ecb3ea8c18824e
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-25 03:40:22 -0500

    File updated at 7/25/2026, 2:10:19 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,18 +1,29 @@
-// Last updated: 7/25/2026, 2:09:29 PM
-1class Solution {
-2    public boolean canJump(int[] nums) {
-3        int maxReach = 0;
-4
-5        for (int i = 0; i < nums.length; i++) {
-6            if (i > maxReach) {
-7                return false;
-8            }
-9            maxReach = Math.max(maxReach, i + nums[i]);
-10            if (maxReach >= nums.length - 1) {
-11                return true;
-12            }
-13        }
-14
-15        return true;
-16    }
-17}
+// Last updated: 7/25/2026, 2:10:19 PM
+1import java.util.*;
+2
+3class Solution {
+4    public int[][] insert(int[][] intervals, int[] newInterval) {
+5        List<int[]> result = new ArrayList<>();
+6        int i = 0;
+7        int n = intervals.length;
+8
+9        while (i < n && intervals[i][1] < newInterval[0]) {
+10            result.add(intervals[i]);
+11            i++;
+12        }
+13
+14        while (i < n && intervals[i][0] <= newInterval[1]) {
+15            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+16            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+17            i++;
+18        }
+19        result.add(newInterval);
+20
+21        while (i < n) {
+22            result.add(intervals[i]);
+23            i++;
+24        }
+25
+26        return result.toArray(new int[result.size()][]);
+27    }
+28}
