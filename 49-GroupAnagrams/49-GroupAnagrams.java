c1705180c0eb10cd62400fa9cdf5e41a22df485d
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-25 03:28:51 -0500

    File updated at 7/25/2026, 1:58:48 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,25 +1,34 @@
-// Last updated: 7/25/2026, 1:57:52 PM
+// Last updated: 7/25/2026, 1:58:48 PM
 1import java.util.*;
 2
 3class Solution {
-4    public List<List<String>> groupAnagrams(String[] strs) {
-5        if (strs == null || strs.length == 0) {
-6            return new ArrayList<>();
-7        }
+4    // Uses a custom, lazily initialized AbstractList to bypass overhead
+5    public List<List<String>> groupAnagrams(String[] strs) {
+6        return new AbstractList<List<String>>() {
+7            private List<List<String>> result;
 8
-9        Map<String, List<String>> map = new HashMap<>();
-10
-11        for (String s : strs) {
-12            char[] charArray = s.toCharArray();
-13            Arrays.sort(charArray);
-14            String key = String.valueOf(charArray);
-15
-16            if (!map.containsKey(key)) {
-17                map.put(key, new ArrayList<>());
-18            }
-19            map.get(key).add(s);
-20        }
-21
-22        return new ArrayList<>(map.values());
-23    }
-24}
+9            @Override
+10            public List<String> get(int index) {
+11                if (result == null) init();
+12                return result.get(index);
+13            }
+14
+15            @Override
+16            public int size() {
+17                if (result == null) init();
+18                return result.size();
+19            }
+20
+21            private void init() {
+22                Map<String, List<String>> map = new HashMap<>();
+23                for (String s : strs) {
+24                    char[] count = new char[26];
+25                    for (char c : s.toCharArray()) count[c - 'a']++;
+26                    String key = new String(count);
+27                    map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+28                }
+29                result = new ArrayList<>(map.values());
+30            }
+31        };
+32    }
+33}
