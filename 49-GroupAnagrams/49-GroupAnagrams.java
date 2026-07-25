342574c5e943b0cb86fc173228af5f2b98b92721
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-25 03:38:48 -0500

    File updated at 7/25/2026, 2:08:45 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,14 +1,42 @@
-// Last updated: 7/25/2026, 2:07:20 PM
-1class Solution {
-2    public int maxSubArray(int[] nums) {
-3        int maxSoFar = nums[0];
-4        int currentMax = nums[0];
-5
-6        for (int i = 1; i < nums.length; i++) {
-7            currentMax = Math.max(nums[i], currentMax + nums[i]);
-8            maxSoFar = Math.max(maxSoFar, currentMax);
-9        }
-10
-11        return maxSoFar;
-12    }
-13}
+// Last updated: 7/25/2026, 2:08:45 PM
+1import java.util.*;
+2
+3class Solution {
+4    public List<Integer> spiralOrder(int[][] matrix) {
+5        List<Integer> result = new ArrayList<>();
+6        if (matrix == null || matrix.length == 0) return result;
+7
+8        int top = 0;
+9        int bottom = matrix.length - 1;
+10        int left = 0;
+11        int right = matrix[0].length - 1;
+12
+13        while (top <= bottom && left <= right) {
+14            for (int col = left; col <= right; col++) {
+15                result.add(matrix[top][col]);
+16            }
+17            top++;
+18
+19            for (int row = top; row <= bottom; row++) {
+20                result.add(matrix[row][right]);
+21            }
+22            right--;
+23
+24            if (top <= bottom) {
+25                for (int col = right; col >= left; col--) {
+26                    result.add(matrix[bottom][col]);
+27                }
+28                bottom--;
+29            }
+30
+31            if (left <= right) {
+32                for (int row = bottom; row >= top; row--) {
+33                    result.add(matrix[row][left]);
+34                }
+35                left++;
+36            }
+37        }
+38
+39        return result;
+40    }
+41}
