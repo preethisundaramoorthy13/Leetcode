e5c12864889237731c8a0101ebc9badda63441be
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-25 03:39:32 -0500

    File updated at 7/25/2026, 2:09:29 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,42 +1,18 @@
-// Last updated: 7/25/2026, 2:08:45 PM
-1import java.util.*;
-2
-3class Solution {
-4    public List<Integer> spiralOrder(int[][] matrix) {
-5        List<Integer> result = new ArrayList<>();
-6        if (matrix == null || matrix.length == 0) return result;
-7
-8        int top = 0;
-9        int bottom = matrix.length - 1;
-10        int left = 0;
-11        int right = matrix[0].length - 1;
-12
-13        while (top <= bottom && left <= right) {
-14            for (int col = left; col <= right; col++) {
-15                result.add(matrix[top][col]);
-16            }
-17            top++;
-18
-19            for (int row = top; row <= bottom; row++) {
-20                result.add(matrix[row][right]);
-21            }
-22            right--;
-23
-24            if (top <= bottom) {
-25                for (int col = right; col >= left; col--) {
-26                    result.add(matrix[bottom][col]);
-27                }
-28                bottom--;
-29            }
-30
-31            if (left <= right) {
-32                for (int row = bottom; row >= top; row--) {
-33                    result.add(matrix[row][left]);
-34                }
-35                left++;
-36            }
-37        }
-38
-39        return result;
-40    }
-41}
+// Last updated: 7/25/2026, 2:09:29 PM
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int maxReach = 0;
+4
+5        for (int i = 0; i < nums.length; i++) {
+6            if (i > maxReach) {
+7                return false;
+8            }
+9            maxReach = Math.max(maxReach, i + nums[i]);
+10            if (maxReach >= nums.length - 1) {
+11                return true;
+12            }
+13        }
+14
+15        return true;
+16    }
+17}
