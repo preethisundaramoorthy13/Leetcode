3ce2bb3d6ae5fdc9cb671ead338799196b00d25c
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-25 03:40:53 -0500

    File updated at 7/25/2026, 2:10:49 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,29 +1,26 @@
-// Last updated: 7/25/2026, 2:10:19 PM
-1import java.util.*;
-2
-3class Solution {
-4    public int[][] insert(int[][] intervals, int[] newInterval) {
-5        List<int[]> result = new ArrayList<>();
-6        int i = 0;
-7        int n = intervals.length;
-8
-9        while (i < n && intervals[i][1] < newInterval[0]) {
-10            result.add(intervals[i]);
-11            i++;
-12        }
-13
-14        while (i < n && intervals[i][0] <= newInterval[1]) {
-15            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-16            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-17            i++;
-18        }
-19        result.add(newInterval);
-20
-21        while (i < n) {
-22            result.add(intervals[i]);
-23            i++;
-24        }
-25
-26        return result.toArray(new int[result.size()][]);
-27    }
-28}
+// Last updated: 7/25/2026, 2:10:49 PM
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int i=0;
+        List<int[]> li=new ArrayList<>();
+        while(i<intervals.length&&intervals[i][1]<newInterval[0]){
+            li.add(intervals[i]);
+            i++;
+        }
+        while(i<intervals.length&&intervals[i][0]<=newInterval[1]){
+             newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+             newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
+             i++;
+        }
+        li.add(newInterval);
+        while(i<intervals.length){
+            li.add(intervals[i]);
+            i++;
+        }int resultSize = li.size();
+        int[][] resultInArray = new int[resultSize][2];
+        for(int j=0;j<resultSize;j++){
+            resultInArray[j] = li.get(j);
+        }
+        return resultInArray;
+    }
+}
