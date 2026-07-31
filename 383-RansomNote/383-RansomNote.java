206c2a63b19723de7539fd3082d9d3dceed8b0fc
Preethi <preethisundaramoorthy1@gmail.com>
2026-07-31 11:12:58 +0700

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,19 @@
+// Last updated: 7/31/2026, 9:42:57 AM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int mLen = magazine.length();
+        int rLen = ransomNote.length();
+
+        if (mLen < rLen) return false;
+
+        int[] freq = new int[26];
+
+        for (char ch : ransomNote.toCharArray()) {
+            int i = magazine.indexOf(ch, freq[ch - 'a']);
+            if (i == -1) return false;
+            freq[ch - 'a'] = i + 1;
+        }
+
+        return true;
+    }
+}
