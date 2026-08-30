eb6dad44d89990b716391c82f2cd14a40787bd6d
Preethi <preethisundaramoorthy1@gmail.com>
2026-08-30 12:58:53 +0700

    File updated at 8/30/2026, 12:58:53 PM

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,38 +1,66 @@
-// Last updated: 8/30/2026, 12:58:45 PM
-1class Solution {
-2    public boolean exist(char[][] board, String word) {
-3        int rows = board.length;
-4        int cols = board[0].length;
-5
-6        for (int r = 0; r < rows; r++) {
-7            for (int c = 0; c < cols; c++) {
-8                if (board[r][c] == word.charAt(0) && dfs(board, word, r, c, 0)) {
-9                    return true;
-10                }
-11            }
-12        }
-13        return false;
-14    }
-15
-16    private boolean dfs(char[][] board, String word, int r, int c, int index) {
-17        if (index == word.length()) {
-18            return true;
-19        }
-20
-21        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != word.charAt(index)) {
-22            return false;
-23        }
-24
-25        char temp = board[r][c];
-26        board[r][c] = '#'; // Mark cell as visited
-27
-28        boolean found = dfs(board, word, r + 1, c, index + 1) ||
-29                        dfs(board, word, r - 1, c, index + 1) ||
-30                        dfs(board, word, r, c + 1, index + 1) ||
-31                        dfs(board, word, r, c - 1, index + 1);
-32
-33        board[r][c] = temp; // Backtrack
-34
-35        return found;
-36    }
-37}
+// Last updated: 8/30/2026, 12:58:53 PM
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        char[] w = word.toCharArray();
+        int len = w.length;
+
+        if (len > m * n) {
+            return false;
+        }
+
+        int[] counts = new int[128];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                counts[board[i][j]]++;
+            }
+        }
+
+        int[] wordCounts = new int[128];
+        for (char c : w) {
+            if (++wordCounts[c] > counts[c]) {
+                return false;
+            }
+        }
+
+        if (counts[w[0]] > counts[w[len - 1]]) {
+            for (int i = 0; i < len / 2; i++) {
+                char temp = w[i];
+                w[i] = w[len - 1 - i];
+                w[len - 1 - i] = temp;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == w[0] && dfs(board, w, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    private boolean dfs(char[][] board, char[] w, int i, int j, int index) {
+        if (index == w.length) {
+            return true;
+        }
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != w[index]) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        boolean found = dfs(board, w, i + 1, j, index + 1)
+                     || dfs(board, w, i - 1, j, index + 1)
+                     || dfs(board, w, i, j + 1, index + 1)
+                     || dfs(board, w, i, j - 1, index + 1);
+
+        board[i][j] = temp;
+        return found;
+    }
+}
