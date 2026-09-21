8c8b8f7879e20119f7256685ce7164f4c65adaa7
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 13:15:13 +0530

    File updated at 9/21/2026, 2:45:13 PM

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
@@ -1,18 +1,23 @@
-// Last updated: 9/21/2026, 2:43:24 PM
+// Last updated: 9/21/2026, 2:45:13 PM
 1class Solution {
-2    public int maxProfit(int[] prices) {
-3        int buy1 = Integer.MIN_VALUE;
-4        int sell1 = 0;
-5        int buy2 = Integer.MIN_VALUE;
-6        int sell2 = 0;
-7
-8        for (int price : prices) {
-9            buy1 = Math.max(buy1, -price);
-10            sell1 = Math.max(sell1, buy1 + price);
-11            buy2 = Math.max(buy2, sell1 - price);
-12            sell2 = Math.max(sell2, buy2 + price);
-13        }
-14
-15        return sell2;
-16    }
-17}
+2    private int maxSum = Integer.MIN_VALUE;
+3
+4    public int maxPathSum(TreeNode root) {
+5        maxGain(root);
+6        return maxSum;
+7    }
+8
+9    private int maxGain(TreeNode node) {
+10        if (node == null) {
+11            return 0;
+12        }
+13
+14        int leftGain = Math.max(maxGain(node.left), 0);
+15        int rightGain = Math.max(maxGain(node.right), 0);
+16
+17        int currentPathSum = node.val + leftGain + rightGain;
+18        maxSum = Math.max(maxSum, currentPathSum);
+19
+20        return node.val + Math.max(leftGain, rightGain);
+21    }
+22}
