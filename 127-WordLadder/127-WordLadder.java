7463a0778438a984f0ca12b47b80c6de18896626
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 13:38:24 +0530

    File updated at 9/21/2026, 3:08:24 PM

--- 127-WordLadder/127-WordLadder.java
+++ 127-WordLadder/127-WordLadder.java
@@ -1,36 +1,57 @@
-// Last updated: 9/21/2026, 3:07:53 PM
-1import java.util.ArrayList;
-2import java.util.List;
-3
-4class Solution {
-5    public List<List<String>> partition(String s) {
-6        int n = s.length();
-7        boolean[][] dp = new boolean[n][n];
-8        for (int i = 0; i < n; i++) {
-9            for (int j = 0; j <= i; j++) {
-10                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
-11                    dp[j][i] = true;
-12                }
-13            }
-14        }
-15
-16        List<List<String>> result = new ArrayList<>();
-17        backtrack(s, 0, dp, new ArrayList<>(), result);
-18        return result;
-19    }
-20
-21    private void backtrack(String s, int start, boolean[][] dp, List<String> current, List<List<String>> result) {
-22        if (start == s.length()) {
-23            result.add(new ArrayList<>(current));
-24            return;
-25        }
-26
-27        for (int end = start; end < s.length(); end++) {
-28            if (dp[start][end]) {
-29                current.add(s.substring(start, end + 1));
-30                backtrack(s, end + 1, dp, current, result);
-31                current.remove(current.size() - 1);
-32            }
-33        }
-34    }
-35}
+// Last updated: 9/21/2026, 3:08:24 PM
+import java.util.AbstractList;
+class Solution {
+    private List<List<String>> myList;
+    public List<List<String>> partition(String s) {
+         return new AbstractList<List<String>>(){
+            @Override 
+            public int size(){
+                init();
+                return myList.size();
+            }
+            @Override
+            public List<String> get (int i){
+                init();
+                return myList.get(i);
+            }
+
+            private void init(){
+                if (myList != null) return;
+                myList = new ArrayList<>();
+                generator(new ArrayList<String>(), s, "", 0);
+            }
+        };
+    }
+
+    private void generator(List<String> list, String s, String a, int i){
+        if (i == s.length()){
+            if (!a.isEmpty() && checker(a)) {
+                list.add(a);
+                myList.add(new ArrayList<>(list));
+                list.remove(list.size()-1);
+                return;
+            }
+            else {
+                return;
+            }
+        } 
+        a+=s.charAt(i);
+        if (checker(a)){
+            list.add(a);
+            generator(list, s, "", i+1);
+            list.remove(list.size()-1);
+        }
+        generator(list, s, a, i+1);
+    }
+
+    private boolean checker(String a){
+        int i = 0;
+        int j = a.length()-1;
+        while (i < j){
+            if (a.charAt(i) != a.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
