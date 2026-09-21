8c55be526d41a72d3a5973e454b0bb5b58f89bef
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 12:00:39 +0530

    File updated at 9/21/2026, 1:30:39 PM

--- 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
+++ 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
@@ -1,28 +1,15 @@
-// Last updated: 9/21/2026, 1:27:09 PM
+// Last updated: 9/21/2026, 1:30:39 PM
 1class Solution {
-2    public boolean isBalanced(TreeNode root) {
-3        return checkHeight(root) != -1;
-4    }
-5
-6    private int checkHeight(TreeNode root) {
-7        if (root == null) {
-8            return 0;
-9        }
-10
-11        int leftHeight = checkHeight(root.left);
-12        if (leftHeight == -1) {
-13            return -1;
-14        }
-15
-16        int rightHeight = checkHeight(root.right);
-17        if (rightHeight == -1) {
-18            return -1;
-19        }
-20
-21        if (Math.abs(leftHeight - rightHeight) > 1) {
-22            return -1;
-23        }
-24
-25        return 1 + Math.max(leftHeight, rightHeight);
-26    }
-27}
+2    public int minDepth(TreeNode root) {
+3        if (root == null) {
+4            return 0;
+5        }
+6        if (root.left == null) {
+7            return minDepth(root.right) + 1;
+8        }
+9        if (root.right == null) {
+10            return minDepth(root.left) + 1;
+11        }
+12        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+13    }
+14}
