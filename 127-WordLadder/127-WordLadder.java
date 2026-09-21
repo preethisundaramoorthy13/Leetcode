608e7ef6d1c835139fcc93e7e9ed823f8496d339
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 13:37:06 +0530

    File updated at 9/21/2026, 3:07:06 PM

--- 127-WordLadder/127-WordLadder.java
+++ 127-WordLadder/127-WordLadder.java
@@ -1,52 +1,33 @@
-// Last updated: 9/21/2026, 3:06:47 PM
-1class Solution {
-2    public void solve(char[][] board) {
-3        if (board == null || board.length == 0) {
-4            return;
-5        }
-6
-7        int m = board.length;
-8        int n = board[0].length;
-9
-10        for (int i = 0; i < m; i++) {
-11            if (board[i][0] == 'O') {
-12                dfs(board, i, 0);
-13            }
-14            if (board[i][n - 1] == 'O') {
-15                dfs(board, i, n - 1);
-16            }
-17        }
-18
-19        for (int j = 0; j < n; j++) {
-20            if (board[0][j] == 'O') {
-21                dfs(board, 0, j);
-22            }
-23            if (board[m - 1][j] == 'O') {
-24                dfs(board, m - 1, j);
-25            }
-26        }
-27
-28        for (int i = 0; i < m; i++) {
-29            for (int j = 0; j < n; j++) {
-30                if (board[i][j] == 'O') {
-31                    board[i][j] = 'X';
-32                } else if (board[i][j] == '#') {
-33                    board[i][j] = 'O';
-34                }
-35            }
-36        }
-37    }
-38
-39    private void dfs(char[][] board, int r, int c) {
-40        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != 'O') {
-41            return;
-42        }
-43
-44        board[r][c] = '#';
-45
-46        dfs(board, r + 1, c);
-47        dfs(board, r - 1, c);
-48        dfs(board, r, c + 1);
-49        dfs(board, r, c - 1);
-50    }
-51}
+// Last updated: 9/21/2026, 3:07:06 PM
+class Solution {
+    private void qRegion(char[][] board, int y, int x){
+        board[y][x] = 'Q';
+        if (x > 0 && board[y][x-1] == 'O') qRegion(board, y, x-1);
+        if (x < board[0].length-1 && board[y][x+1] == 'O') qRegion(board, y, x+1);
+        if (y > 0 && board[y-1][x] == 'O') qRegion(board, y-1, x);
+        if (y < board.length-1 && board[y+1][x] == 'O') qRegion(board, y+1, x);
+    }
+
+    private void replaceOnBoard(char[][] board, char oldC, char newC){
+        for (int y = 0; y < board.length; y++){
+            for (int x = 0; x < board[0].length; x++){
+                if (board[y][x] == oldC) board[y][x] = newC;
+            }
+        }
+    }
+
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < n; i++){ // top and bottom
+            if (board[0][i] == 'O') qRegion(board, 0, i);
+            if (board[m-1][i] == 'O') qRegion(board, m-1, i);
+        }
+        for (int i = 1; i < m-1; i++){ // left and right
+            if (board[i][0] == 'O') qRegion(board, i, 0);
+            if (board[i][n-1] == 'O') qRegion(board, i, n-1);
+        }
+        replaceOnBoard(board, 'O', 'X');
+        replaceOnBoard(board, 'Q', 'O');
+    }
+}
