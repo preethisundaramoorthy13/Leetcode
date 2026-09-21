2f4bccb8b1d9305994fa28587501f20ac5b2b94e
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 16:27:21 +0530

    File updated at 9/21/2026, 5:57:21 PM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,21 +1,49 @@
-// Last updated: 9/21/2026, 5:56:22 PM
-1class Solution {
-2    public int findMin(int[] nums) {
-3        int low = 0;
-4        int high = nums.length - 1;
-5
-6        while (low < high) {
-7            int mid = low + (high - low) / 2;
+// Last updated: 9/21/2026, 5:57:21 PM
+1import java.util.Arrays;
+2
+3class Solution {
+4    public int maximumGap(int[] nums) {
+5        if (nums == null || nums.length < 2) {
+6            return 0;
+7        }
 8
-9            if (nums[mid] < nums[high]) {
-10                high = mid;
-11            } else if (nums[mid] > nums[high]) {
-12                low = mid + 1;
-13            } else {
-14                high--;
-15            }
-16        }
-17
-18        return nums[low];
-19    }
-20}
+9        int min = nums[0];
+10        int max = nums[0];
+11        for (int num : nums) {
+12            min = Math.min(min, num);
+13            max = Math.max(max, num);
+14        }
+15
+16        if (min == max) {
+17            return 0;
+18        }
+19
+20        int n = nums.length;
+21        int bucketSize = Math.max(1, (max - min) / (n - 1));
+22        int bucketCount = (max - min) / bucketSize + 1;
+23
+24        int[] minBucket = new int[bucketCount];
+25        int[] maxBucket = new int[bucketCount];
+26        Arrays.fill(minBucket, Integer.MAX_VALUE);
+27        Arrays.fill(maxBucket, Integer.MIN_VALUE);
+28
+29        for (int num : nums) {
+30            int idx = (num - min) / bucketSize;
+31            minBucket[idx] = Math.min(minBucket[idx], num);
+32            maxBucket[idx] = Math.max(maxBucket[idx], num);
+33        }
+34
+35        int maxGap = 0;
+36        int prevMax = min;
+37
+38        for (int i = 0; i < bucketCount; i++) {
+39            if (minBucket[i] == Integer.MAX_VALUE) {
+40                continue;
+41            }
+42            maxGap = Math.max(maxGap, minBucket[i] - prevMax);
+43            prevMax = maxBucket[i];
+44        }
+45
+46        return maxGap;
+47    }
+48}
