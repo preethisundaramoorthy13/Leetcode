f39ba4ac70860bab26178b1b3596b44f869dca43
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 16:01:32 +0530

    Create 2651-CalculateDelayedArrivalTime

---
+++ 2651-CalculateDelayedArrivalTime/2651-CalculateDelayedArrivalTime.java
@@ -0,0 +1,6 @@
+// Last updated: 9/21/2026, 5:31:32 PM
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        return (arrivalTime+delayedTime)%24;
+    }
+}
