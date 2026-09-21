f30e7dd8c69b2a9888b64b10b806ab96da01f7b1
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 15:17:21 +0530

    File updated at 9/21/2026, 4:47:21 PM

--- 127-WordLadder/127-WordLadder.java
+++ 127-WordLadder/127-WordLadder.java
@@ -1,23 +1,20 @@
-// Last updated: 9/21/2026, 3:42:06 PM
+// Last updated: 9/21/2026, 4:47:21 PM
 1class Solution {
-2    private Node[] visited = new Node[101];
-3
-4    public Node cloneGraph(Node node) {
-5        if (node == null) {
-6            return null;
-7        }
-8
-9        if (visited[node.val] != null) {
-10            return visited[node.val];
-11        }
-12
-13        Node clone = new Node(node.val);
-14        visited[node.val] = clone;
-15
-16        for (Node neighbor : node.neighbors) {
-17            clone.neighbors.add(cloneGraph(neighbor));
-18        }
-19
-20        return clone;
-21    }
-22}
+2    public int canCompleteCircuit(int[] gas, int[] cost) {
+3        int total = 0;
+4        int tank = 0;
+5        int start = 0;
+6
+7        for (int i = 0; i < gas.length; i++) {
+8            int diff = gas[i] - cost[i];
+9            total += diff;
+10            tank += diff;
+11            if (tank < 0) {
+12                start = i + 1;
+13                tank = 0;
+14            }
+15        }
+16
+17        return total >= 0 ? start : -1;
+18    }
+19}
