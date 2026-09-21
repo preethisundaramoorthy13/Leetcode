b7bbdd52de10c7723003e26cb7c58bb672136509
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 11:50:42 +0530

    File updated at 9/21/2026, 1:20:42 PM

--- 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
+++ 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
@@ -1,27 +1,31 @@
-// Last updated: 9/21/2026, 1:20:02 PM
+// Last updated: 9/21/2026, 1:20:42 PM
 1class Solution {
-2    public boolean isInterleave(String s1, String s2, String s3) {
-3        if (s1.length() + s2.length() != s3.length()) {
-4            return false;
-5        }
-6        return dfs(s1.toCharArray(), s2.toCharArray(), s3.toCharArray(), 0, 0, 0, new boolean[s1.length() + 1][s2.length() + 1]);
-7    }
-8
-9    private boolean dfs(char[] c1, char[] c2, char[] c3, int i, int j, int k, boolean[][] invalid) {
-10        if (invalid[i][j]) {
-11            return false;
-12        }
-13        if (k == c3.length) {
-14            return true;
-15        }
-16
-17        boolean valid = (i < c1.length && c1[i] == c3[k] && dfs(c1, c2, c3, i + 1, j, k + 1, invalid)) ||
-18                        (j < c2.length && c2[j] == c3[k] && dfs(c1, c2, c3, i, j + 1, k + 1, invalid));
+2    private TreeNode first;
+3    private TreeNode second;
+4    private TreeNode prev;
+5
+6    public void recoverTree(TreeNode root) {
+7        inorder(root);
+8        int temp = first.val;
+9        first.val = second.val;
+10        second.val = temp;
+11    }
+12
+13    private void inorder(TreeNode root) {
+14        if (root == null) {
+15            return;
+16        }
+17
+18        inorder(root.left);
 19
-20        if (!valid) {
-21            invalid[i][j] = true;
-22        }
-23
-24        return valid;
-25    }
-26}
+20        if (prev != null && prev.val > root.val) {
+21            if (first == null) {
+22                first = prev;
+23            }
+24            second = root;
+25        }
+26        prev = root;
+27
+28        inorder(root.right);
+29    }
+30}
