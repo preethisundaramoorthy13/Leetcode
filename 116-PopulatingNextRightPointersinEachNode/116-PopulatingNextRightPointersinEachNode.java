c4177d91e961d0f67b739db602eb9333c9faeea6
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 13:06:55 +0530

    File updated at 9/21/2026, 2:36:55 PM

--- 116-PopulatingNextRightPointersinEachNode/116-PopulatingNextRightPointersinEachNode.java
+++ 116-PopulatingNextRightPointersinEachNode/116-PopulatingNextRightPointersinEachNode.java
@@ -1,23 +1,16 @@
-// Last updated: 9/21/2026, 2:36:05 PM
+// Last updated: 9/21/2026, 2:36:55 PM
 1class Solution {
-2    public Node connect(Node root) {
-3        Node head = root;
-4        while (head != null) {
-5            Node dummy = new Node(0);
-6            Node temp = dummy;
-7            while (head != null) {
-8                if (head.left != null) {
-9                    temp.next = head.left;
-10                    temp = temp.next;
-11                }
-12                if (head.right != null) {
-13                    temp.next = head.right;
-14                    temp = temp.next;
-15                }
-16                head = head.next;
-17            }
-18            head = dummy.next;
-19        }
-20        return root;
-21    }
-22}
+2    public int minimumTotal(List<List<Integer>> triangle) {
+3        int n = triangle.size();
+4        int[] dp = new int[n + 1];
+5
+6        for (int row = n - 1; row >= 0; row--) {
+7            List<Integer> currRow = triangle.get(row);
+8            for (int i = 0; i < currRow.size(); i++) {
+9                dp[i] = currRow.get(i) + Math.min(dp[i], dp[i + 1]);
+10            }
+11        }
+12
+13        return dp[0];
+14    }
+15}
