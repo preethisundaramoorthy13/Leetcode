865a0606d63a354742c749ad6b37426d361622a6
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 16:03:04 +0530

    Create 1545-FindKthBitInNthBinaryString

---
+++ 1545-FindKthBitInNthBinaryString/1545-FindKthBitInNthBinaryString.java
@@ -0,0 +1,18 @@
+// Last updated: 9/21/2026, 5:33:04 PM
+class Solution {
+    public char findKthBit(int n, int k) {
+        if (n == 1) return '0';
+        
+        int len = (1 << n) - 1;
+        int mid = len / 2 + 1;
+        
+        if (k == mid) {
+            return '1';
+        } else if (k < mid) {
+            return findKthBit(n - 1, k);
+        } else {
+            char ch = findKthBit(n - 1, len - k + 1);
+            return ch == '0' ? '1' : '0';
+        }
+    }
+}
