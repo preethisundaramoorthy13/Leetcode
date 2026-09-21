11734a7828b0fd712b2e9305e27b88847c150519
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 11:52:11 +0530

    File updated at 9/21/2026, 1:22:11 PM

--- 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
+++ 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
@@ -1,9 +1,25 @@
-// Last updated: 9/21/2026, 1:21:22 PM
+// Last updated: 9/21/2026, 1:22:11 PM
 1class Solution {
-2    public int maxDepth(TreeNode root) {
-3        if (root == null) {
-4            return 0;
-5        }
-6        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+2    private int in = 0;
+3    private int pre = 0;
+4
+5    public TreeNode buildTree(int[] preorder, int[] inorder) {
+6        return build(preorder, inorder, Integer.MIN_VALUE);
 7    }
-8}
+8
+9    private TreeNode build(int[] preorder, int[] inorder, int stop) {
+10        if (pre >= preorder.length) {
+11            return null;
+12        }
+13        if (inorder[in] == stop) {
+14            in++;
+15            return null;
+16        }
+17
+18        TreeNode root = new TreeNode(preorder[pre++]);
+19        root.left = build(preorder, inorder, root.val);
+20        root.right = build(preorder, inorder, stop);
+21
+22        return root;
+23    }
+24}
