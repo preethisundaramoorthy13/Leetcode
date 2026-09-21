73faf64e2efcd24845d35db54e4fca25e0738533
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 16:03:12 +0530

    Create 1484-GroupSoldProductsByTheDate

---
+++ 1484-GroupSoldProductsByTheDate/1484-GroupSoldProductsByTheDate.sql
@@ -0,0 +1,9 @@
+-- Last updated: 9/21/2026, 5:33:12 PM
+# Write your MySQL query statement below
+SELECT 
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date;
