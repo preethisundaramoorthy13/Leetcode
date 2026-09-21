78000eed15e68a668ef069c51d49b3c904a252fb
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 16:29:36 +0530

    File updated at 9/21/2026, 5:59:36 PM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,35 +1,45 @@
-// Last updated: 9/21/2026, 5:58:01 PM
-1class Solution {
-2    public int compareVersion(String version1, String version2) {
-3        int n1 = version1.length();
-4        int n2 = version2.length();
-5        int i = 0;
-6        int j = 0;
-7
-8        while (i < n1 || j < n2) {
-9            int num1 = 0;
-10            while (i < n1 && version1.charAt(i) != '.') {
-11                num1 = num1 * 10 + (version1.charAt(i) - '0');
-12                i++;
-13            }
-14
-15            int num2 = 0;
-16            while (j < n2 && version2.charAt(j) != '.') {
-17                num2 = num2 * 10 + (version2.charAt(j) - '0');
-18                j++;
-19            }
-20
-21            if (num1 < num2) {
-22                return -1;
-23            }
-24            if (num1 > num2) {
-25                return 1;
-26            }
-27
-28            i++;
-29            j++;
-30        }
-31
-32        return 0;
-33    }
-34}
+// Last updated: 9/21/2026, 5:59:36 PM
+1import java.util.HashMap;
+2import java.util.Map;
+3
+4class Solution {
+5    public String fractionToDecimal(int numerator, int denominator) {
+6        if (numerator == 0) {
+7            return "0";
+8        }
+9
+10        StringBuilder sb = new StringBuilder();
+11
+12        if ((numerator < 0) ^ (denominator < 0)) {
+13            sb.append("-");
+14        }
+15
+16        long num = Math.abs((long) numerator);
+17        long den = Math.abs((long) denominator);
+18
+19        sb.append(num / den);
+20        long remainder = num % den;
+21
+22        if (remainder == 0) {
+23            return sb.toString();
+24        }
+25
+26        sb.append(".");
+27        Map<Long, Integer> map = new HashMap<>();
+28
+29        while (remainder != 0) {
+30            if (map.containsKey(remainder)) {
+31                sb.insert(map.get(remainder), "(");
+32                sb.append(")");
+33                break;
+34            }
+35
+36            map.put(remainder, sb.length());
+37            remainder *= 10;
+38            sb.append(remainder / den);
+39            remainder %= den;
+40        }
+41
+42        return sb.toString();
+43    }
+44}
