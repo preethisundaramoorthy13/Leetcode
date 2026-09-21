a86b7aba3dc10cf199c85320271afaffffcfd73e
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 12:32:06 +0530

    File updated at 9/21/2026, 2:02:06 PM

--- 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
+++ 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
@@ -1,28 +1,24 @@
-// Last updated: 9/21/2026, 2:01:10 PM
+// Last updated: 9/21/2026, 2:02:06 PM
 1class Solution {
-2    public int numDistinct(String s, String t) {
-3        int m = s.length();
-4        int n = t.length();
-5        if (m < n) {
-6            return 0;
-7        }
+2    public Node connect(Node root) {
+3        if (root == null) {
+4            return null;
+5        }
+6
+7        Node curr = root;
 8
-9        char[] sChars = s.toCharArray();
-10        char[] tChars = t.toCharArray();
-11
-12        int[] dp = new int[n + 1];
-13        dp[0] = 1;
-14
-15        for (int i = 0; i < m; i++) {
-16            int maxJ = Math.min(i, n - 1);
-17            int minJ = Math.max(0, n - m + i);
-18            for (int j = maxJ; j >= minJ; j--) {
-19                if (sChars[i] == tChars[j]) {
-20                    dp[j + 1] += dp[j];
-21                }
-22            }
-23        }
-24
-25        return dp[n];
-26    }
-27}
+9        while (curr.left != null) {
+10            Node head = curr;
+11            while (head != null) {
+12                head.left.next = head.right;
+13                if (head.next != null) {
+14                    head.right.next = head.next.left;
+15                }
+16                head = head.next;
+17            }
+18            curr = curr.left;
+19        }
+20
+21        return root;
+22    }
+23}
