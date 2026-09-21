644ce16900a552fad33b193dd3a1b050bd24a643
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 11:53:46 +0530

    File updated at 9/21/2026, 1:23:46 PM

--- 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
+++ 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
@@ -1,25 +1,27 @@
-// Last updated: 9/21/2026, 1:22:11 PM
+// Last updated: 9/21/2026, 1:23:46 PM
 1class Solution {
-2    private int in = 0;
-3    private int pre = 0;
+2    private int post;
+3    private int in;
 4
-5    public TreeNode buildTree(int[] preorder, int[] inorder) {
-6        return build(preorder, inorder, Integer.MIN_VALUE);
-7    }
-8
-9    private TreeNode build(int[] preorder, int[] inorder, int stop) {
-10        if (pre >= preorder.length) {
-11            return null;
-12        }
-13        if (inorder[in] == stop) {
-14            in++;
-15            return null;
-16        }
-17
-18        TreeNode root = new TreeNode(preorder[pre++]);
-19        root.left = build(preorder, inorder, root.val);
-20        root.right = build(preorder, inorder, stop);
-21
-22        return root;
-23    }
-24}
+5    public TreeNode buildTree(int[] inorder, int[] postorder) {
+6        post = postorder.length - 1;
+7        in = inorder.length - 1;
+8        return build(inorder, postorder, Integer.MIN_VALUE);
+9    }
+10
+11    private TreeNode build(int[] inorder, int[] postorder, int stop) {
+12        if (post < 0) {
+13            return null;
+14        }
+15        if (inorder[in] == stop) {
+16            in--;
+17            return null;
+18        }
+19
+20        TreeNode root = new TreeNode(postorder[post--]);
+21        root.right = build(inorder, postorder, root.val);
+22        root.left = build(inorder, postorder, stop);
+23
+24        return root;
+25    }
+26}
