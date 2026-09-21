8b7b76c40c58fa8a26cd3bd6b2425c4cbe014316
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 11:56:35 +0530

    File updated at 9/21/2026, 1:26:35 PM

--- 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
+++ 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
@@ -1,38 +1,28 @@
-// Last updated: 9/21/2026, 1:25:47 PM
+// Last updated: 9/21/2026, 1:26:35 PM
 1class Solution {
-2    private ListNode head;
-3
-4    public TreeNode sortedListToBST(ListNode head) {
-5        this.head = head;
-6        int length = getLength(head);
-7        return buildTree(0, length - 1);
-8    }
-9
-10    private int getLength(ListNode head) {
-11        int len = 0;
-12        while (head != null) {
-13            len++;
-14            head = head.next;
-15        }
-16        return len;
-17    }
-18
-19    private TreeNode buildTree(int left, int right) {
-20        if (left > right) {
-21            return null;
-22        }
-23
-24        int mid = left + (right - left) / 2;
-25
-26        TreeNode leftChild = buildTree(left, mid - 1);
-27
-28        TreeNode root = new TreeNode(head.val);
-29        root.left = leftChild;
-30
-31        head = head.next;
-32
-33        root.right = buildTree(mid + 1, right);
-34
-35        return root;
-36    }
-37}
+2    public boolean isBalanced(TreeNode root) {
+3        return checkHeight(root) != -1;
+4    }
+5
+6    private int checkHeight(TreeNode root) {
+7        if (root == null) {
+8            return 0;
+9        }
+10
+11        int leftHeight = checkHeight(root.left);
+12        if (leftHeight == -1) {
+13            return -1;
+14        }
+15
+16        int rightHeight = checkHeight(root.right);
+17        if (rightHeight == -1) {
+18            return -1;
+19        }
+20
+21        if (Math.abs(leftHeight - rightHeight) > 1) {
+22            return -1;
+23        }
+24
+25        return 1 + Math.max(leftHeight, rightHeight);
+26    }
+27}
