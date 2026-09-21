accbfd306e48bb44a077306c94d241e644039da6
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 16:09:19 +0530

    Create 129-SumRootToLeafNumbers

---
+++ 129-SumRootToLeafNumbers/129-SumRootToLeafNumbers.java
@@ -0,0 +1,20 @@
+// Last updated: 9/21/2026, 5:39:19 PM
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, int currentSum) {
+        if (node == null) {
+            return 0;
+        }
+
+        currentSum = currentSum * 10 + node.val;
+
+        if (node.left == null && node.right == null) {
+            return currentSum;
+        }
+
+        return dfs(node.left, currentSum) + dfs(node.right, currentSum);
+    }
+}
