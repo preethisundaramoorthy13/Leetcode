612fe40a4bf63c4691ad3ac14c3ea1f10e7a5f81
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 16:05:34 +0530

    Create 1068-ProductSalesAnalysisI

---
+++ 1068-ProductSalesAnalysisI/1068-ProductSalesAnalysisI.sql
@@ -0,0 +1,5 @@
+-- Last updated: 9/21/2026, 5:35:34 PM
+# Write your MySQL query statement below
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+JOIN Product p ON s.product_id = p.product_id;
