ea78dea02670c691f6482288722cb49bfd31d4d9
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 12:11:19 +0530

    File updated at 9/21/2026, 1:41:19 PM

--- 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
+++ 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
@@ -1,33 +1,12 @@
-// Last updated: 9/21/2026, 1:37:11 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    int min = 100001;
-    public int minDepth(TreeNode root) {
-        if(root==null)   return 0;
-        dfs(root, 0);
-        return min;
-    }
-    private void dfs(TreeNode root, int depth) {
-        if(++depth>=min) return;
-        if(root.left==null && root.right==null) {
-            min = depth;
-            return;
-        }
-        if(root.left!=null && (root.right==null||root.right.left!=null||root.right.right!=null))           dfs(root.left, depth);
-        if(root.right!=null)    dfs(root.right, depth);
-    }
-}
+// Last updated: 9/21/2026, 1:41:19 PM
+1class Solution {
+2    public boolean hasPathSum(TreeNode root, int targetSum) {
+3        if (root == null) {
+4            return false;
+5        }
+6        if (root.left == null && root.right == null) {
+7            return targetSum == root.val;
+8        }
+9        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+10    }
+11}
