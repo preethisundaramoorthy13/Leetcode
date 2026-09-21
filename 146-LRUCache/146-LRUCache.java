af0fa38a4d3bfba6c9339d5916508aeee786474b
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 16:25:33 +0530

    File updated at 9/21/2026, 5:55:33 PM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,31 +1,17 @@
-// Last updated: 9/21/2026, 5:51:52 PM
+// Last updated: 9/21/2026, 5:55:33 PM
 1class Solution {
-2    public String reverseWords(String s) {
-3        char[] a = s.toCharArray();
-4        int n = a.length;
-5        char[] res = new char[n + 1];
-6        int resIdx = 0;
-7        int i = n - 1;
-8
-9        while (i >= 0) {
-10            while (i >= 0 && a[i] == ' ') {
-11                i--;
-12            }
-13            if (i < 0) {
-14                break;
-15            }
-16            int j = i;
-17            while (i >= 0 && a[i] != ' ') {
-18                i--;
-19            }
-20            if (resIdx > 0) {
-21                res[resIdx++] = ' ';
-22            }
-23            for (int k = i + 1; k <= j; k++) {
-24                res[resIdx++] = a[k];
-25            }
-26        }
-27
-28        return new String(res, 0, resIdx);
-29    }
-30}
+2    public int maxProduct(int[] nums) {
+3        int n = nums.length;
+4        int max = nums[0];
+5        int l = 0;
+6        int r = 0;
+7
+8        for (int i = 0; i < n; i++) {
+9            l = (l == 0 ? 1 : l) * nums[i];
+10            r = (r == 0 ? 1 : r) * nums[n - 1 - i];
+11            max = Math.max(max, Math.max(l, r));
+12        }
+13
+14        return max;
+15    }
+16}
