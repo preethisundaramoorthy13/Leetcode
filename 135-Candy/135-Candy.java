825221f67422e2c5c1e17daeac6537a3e7655875
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 15:57:40 +0530

    File updated at 9/21/2026, 5:27:40 PM

--- 135-Candy/135-Candy.java
+++ 135-Candy/135-Candy.java
@@ -1,56 +1,27 @@
-// Last updated: 9/21/2026, 5:26:50 PM
-1import java.util.ArrayList;
-2import java.util.List;
-3
-4class Solution {
-5    public List<String> wordBreak(String s, List<String> wordDict) {
-6        TrieNode root = buildTrie(wordDict);
-7        List<String> result = new ArrayList<>();
-8        dfs(s, 0, root, new StringBuilder(), result);
-9        return result;
-10    }
-11
-12    private void dfs(String s, int index, TrieNode root, StringBuilder current, List<String> result) {
-13        if (index == s.length()) {
-14            result.add(current.substring(0, current.length() - 1));
-15            return;
-16        }
-17
-18        TrieNode curr = root;
-19        int len = current.length();
-20
-21        for (int i = index; i < s.length(); i++) {
-22            int charIndex = s.charAt(i) - 'a';
-23            if (curr.children[charIndex] == null) {
-24                break;
-25            }
-26            curr = curr.children[charIndex];
-27            if (curr.isWord) {
-28                current.append(s, index, i + 1).append(" ");
-29                dfs(s, i + 1, root, current, result);
-30                current.setLength(len);
-31            }
-32        }
-33    }
-34
-35    private TrieNode buildTrie(List<String> wordDict) {
-36        TrieNode root = new TrieNode();
-37        for (String word : wordDict) {
-38            TrieNode curr = root;
-39            for (char c : word.toCharArray()) {
-40                int index = c - 'a';
-41                if (curr.children[index] == null) {
-42                    curr.children[index] = new TrieNode();
-43                }
-44                curr = curr.children[index];
-45            }
-46            curr.isWord = true;
-47        }
-48        return root;
-49    }
-50
-51    private static class TrieNode {
-52        TrieNode[] children = new TrieNode[26];
-53        boolean isWord;
-54    }
-55}
+// Last updated: 9/21/2026, 5:27:40 PM
+1public class Solution {
+2    public ListNode detectCycle(ListNode head) {
+3        if (head == null || head.next == null) {
+4            return null;
+5        }
+6
+7        ListNode slow = head;
+8        ListNode fast = head;
+9
+10        while (fast != null && fast.next != null) {
+11            slow = slow.next;
+12            fast = fast.next.next;
+13
+14            if (slow == fast) {
+15                ListNode entry = head;
+16                while (entry != slow) {
+17                    entry = entry.next;
+18                    slow = slow.next;
+19                }
+20                return entry;
+21            }
+22        }
+23
+24        return null;
+25    }
+26}
