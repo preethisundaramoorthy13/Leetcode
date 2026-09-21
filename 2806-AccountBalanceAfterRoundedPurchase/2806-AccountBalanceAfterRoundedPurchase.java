835814283eb36390d9ac7d13976a8f325d1eb2f6
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 16:01:18 +0530

    Create 2806-AccountBalanceAfterRoundedPurchase

---
+++ 2806-AccountBalanceAfterRoundedPurchase/2806-AccountBalanceAfterRoundedPurchase.java
@@ -0,0 +1,7 @@
+// Last updated: 9/21/2026, 5:31:19 PM
+class Solution {
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+        return 100 - ((purchaseAmount + 5)/10)*10;
+
+    }
+}
