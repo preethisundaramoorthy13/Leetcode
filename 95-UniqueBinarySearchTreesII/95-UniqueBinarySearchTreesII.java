c3e922198a57a8c51f530dce491b8807101f192f
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-21 11:54:33 +0530

    File updated at 9/21/2026, 1:24:33 PM

--- 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
+++ 95-UniqueBinarySearchTreesII/95-UniqueBinarySearchTreesII.java
@@ -1,27 +1,38 @@
-// Last updated: 9/21/2026, 1:23:46 PM
+// Last updated: 9/21/2026, 1:24:33 PM
 1class Solution {
-2    private int post;
-3    private int in;
-4
-5    public TreeNode buildTree(int[] inorder, int[] postorder) {
-6        post = postorder.length - 1;
-7        in = inorder.length - 1;
-8        return build(inorder, postorder, Integer.MIN_VALUE);
-9    }
-10
-11    private TreeNode build(int[] inorder, int[] postorder, int stop) {
-12        if (post < 0) {
-13            return null;
-14        }
-15        if (inorder[in] == stop) {
-16            in--;
-17            return null;
-18        }
-19
-20        TreeNode root = new TreeNode(postorder[post--]);
-21        root.right = build(inorder, postorder, root.val);
-22        root.left = build(inorder, postorder, stop);
+2    private ListNode head;
+3
+4    public TreeNode sortedListToBST(ListNode head) {
+5        this.head = head;
+6        int length = getLength(head);
+7        return buildTree(0, length - 1);
+8    }
+9
+10    private int getLength(ListNode head) {
+11        int len = 0;
+12        while (head != null) {
+13            len++;
+14            head = head.next;
+15        }
+16        return len;
+17    }
+18
+19    private TreeNode buildTree(int left, int right) {
+20        if (left > right) {
+21            return null;
+22        }
 23
-24        return root;
-25    }
-26}
+24        int mid = left + (right - left) / 2;
+25
+26        TreeNode leftChild = buildTree(left, mid - 1);
+27
+28        TreeNode root = new TreeNode(head.val);
+29        root.left = leftChild;
+30
+31        head = head.next;
+32
+33        root.right = buildTree(mid + 1, right);
+34
+35        return root;
+36    }
+37}
