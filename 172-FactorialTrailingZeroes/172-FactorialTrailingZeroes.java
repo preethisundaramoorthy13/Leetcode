51b2ab94d660b8b1e1e2b5edbefd7401538c8e8b
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-22 09:34:00 +0530

    File updated at 9/22/2026, 9:34:01 AM

--- 172-FactorialTrailingZeroes/172-FactorialTrailingZeroes.java
+++ 172-FactorialTrailingZeroes/172-FactorialTrailingZeroes.java
@@ -1,31 +1,26 @@
-// Last updated: 9/22/2026, 9:33:40 AM
-1class Solution {
-2    public int calculateMinimumHP(int[][] dungeon) {
-3        int m = dungeon.length;
-4        int n = dungeon[0].length;
-5        
-6        // dp[i][j] represents the minimum HP required when entering cell (i, j)
-7        int[][] dp = new int[m + 1][n + 1];
-8        
-9        // Initialize table with infinity to handle boundary constraints
-10        for (int i = 0; i <= m; i++) {
-11            for (int j = 0; j <= n; j++) {
-12                dp[i][j] = Integer.MAX_VALUE;
-13            }
-14        }
-15        
-16        // Base cases: neighboring boundaries of the destination cell require at least 1 HP
-17        dp[m][n - 1] = 1;
-18        dp[m - 1][n] = 1;
-19        
-20        // Bottom-up computation from bottom-right to top-left
-21        for (int i = m - 1; i >= 0; i--) {
-22            for (int j = n - 1; j >= 0; j--) {
-23                int minHealthOnExit = Math.min(dp[i + 1][j], dp[i][j + 1]);
-24                dp[i][j] = Math.max(1, minHealthOnExit - dungeon[i][j]);
-25            }
-26        }
-27        
-28        return dp[0][0];
-29    }
-30}
+// Last updated: 9/22/2026, 9:34:01 AM
+class Solution {
+    Integer dp[][];
+    private int solve(int i, int j, int arr[][]) {
+        int m = arr.length;
+        int n = arr[0].length;
+        if(i >= m || j >= n) return (int)(-1e7);
+        if(i == m-1 && j == n-1) return Math.min(0, arr[i][j]); //if positive hp then we dont need that
+        
+        if(dp[i][j] !=null) return dp[i][j];
+
+        int right = solve(i,j+1, arr);
+        int down = solve(i+1, j, arr);
+
+        int better = Math.max(right, down); //less negative is better path
+        return dp[i][j] = Math.min(better + arr[i][j], 0);
+    }
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        dp = new Integer[m][n];
+        int ans = solve(0,0, dungeon);
+        return Math.abs(ans)+1;
+    }
+
+}
