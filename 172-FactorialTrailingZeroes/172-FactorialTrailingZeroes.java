9aa1bbc504422aa3d107f72fe20c3e79a1874fdd
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-22 09:32:44 +0530

    File updated at 9/22/2026, 9:32:45 AM

--- 172-FactorialTrailingZeroes/172-FactorialTrailingZeroes.java
+++ 172-FactorialTrailingZeroes/172-FactorialTrailingZeroes.java
@@ -1,11 +1,44 @@
-// Last updated: 9/22/2026, 9:11:53 AM
-1class Solution {
-2    public int trailingZeroes(int n) {
-3        int count = 0;
-4        while (n > 0) {
-5            n /= 5;
-6            count += n;
-7        }
-8        return count;
-9    }
-10}
+// Last updated: 9/22/2026, 9:32:45 AM
+1import java.util.ArrayDeque;
+2import java.util.Deque;
+3
+4/**
+5 * Definition for a binary tree node.
+6 * public class TreeNode {
+7 *     int val;
+8 *     TreeNode left;
+9 *     TreeNode right;
+10 *     TreeNode() {}
+11 *     TreeNode(int val) { this.val = val; }
+12 *     TreeNode(int val, TreeNode left, TreeNode right) {
+13 *         this.val = val;
+14 *         this.left = left;
+15 *         this.right = right;
+16 *     }
+17 * }
+18 */
+19class BSTIterator {
+20    private Deque<TreeNode> stack;
+21
+22    public BSTIterator(TreeNode root) {
+23        stack = new ArrayDeque<>();
+24        pushAllLeft(root);
+25    }
+26    
+27    public int next() {
+28        TreeNode currentNode = stack.pop();
+29        pushAllLeft(currentNode.right);
+30        return currentNode.val;
+31    }
+32    
+33    public boolean hasNext() {
+34        return !stack.isEmpty();
+35    }
+36
+37    private void pushAllLeft(TreeNode node) {
+38        while (node != null) {
+39            stack.push(node);
+40            node = node.left;
+41        }
+42    }
+43}
