9c06b9d87d52b39ef9fe0d846c41232ee42fe3b6
Preethi <preethisundaramoorthy1@gmail.com>
2026-09-22 09:34:52 +0530

    File updated at 9/22/2026, 9:34:53 AM

--- 172-FactorialTrailingZeroes/172-FactorialTrailingZeroes.java
+++ 172-FactorialTrailingZeroes/172-FactorialTrailingZeroes.java
@@ -1,26 +1,25 @@
-// Last updated: 9/22/2026, 9:34:01 AM
-class Solution {
-    Integer dp[][];
-    private int solve(int i, int j, int arr[][]) {
-        int m = arr.length;
-        int n = arr[0].length;
-        if(i >= m || j >= n) return (int)(-1e7);
-        if(i == m-1 && j == n-1) return Math.min(0, arr[i][j]); //if positive hp then we dont need that
-        
-        if(dp[i][j] !=null) return dp[i][j];
-
-        int right = solve(i,j+1, arr);
-        int down = solve(i+1, j, arr);
-
-        int better = Math.max(right, down); //less negative is better path
-        return dp[i][j] = Math.min(better + arr[i][j], 0);
-    }
-    public int calculateMinimumHP(int[][] dungeon) {
-        int m = dungeon.length;
-        int n = dungeon[0].length;
-        dp = new Integer[m][n];
-        int ans = solve(0,0, dungeon);
-        return Math.abs(ans)+1;
-    }
-
-}
+// Last updated: 9/22/2026, 9:34:53 AM
+1import java.util.ArrayList;
+2import java.util.HashSet;
+3import java.util.List;
+4import java.util.Set;
+5
+6class Solution {
+7    public List<String> findRepeatedDnaSequences(String s) {
+8        if (s == null || s.length() <= 10) {
+9            return new ArrayList<>();
+10        }
+11        
+12        Set<String> seen = new HashSet<>();
+13        Set<String> repeated = new HashSet<>();
+14        
+15        for (int i = 0; i <= s.length() - 10; i++) {
+16            String sequence = s.substring(i, i + 10);
+17            if (!seen.add(sequence)) {
+18                repeated.add(sequence);
+19            }
+20        }
+21        
+22        return new ArrayList<>(repeated);
+23    }
+24}
